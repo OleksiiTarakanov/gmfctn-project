8fe8fd26cb7c97d6c0987a72b445fa58c6d2ad69
Alex <siricuster@gmail.com>
2021-03-26 17:12:29 -0400

    header+routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { MaterialModule } from './material/material.module';
-// import { NavigationComponent } from './navigation/navigation.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { NavComponent } from './components/header/nav/nav.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BadgesComponent } from './components/badges/badges.component';
+import { SidebarComponent } from './components/header/sidebar/sidebar.component';
+
+const routes: Routes = [
+  {
+    path: 'Dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'Badges',
+    component: BadgesComponent
+  }
+]
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    NavComponent,
+    DashboardComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
