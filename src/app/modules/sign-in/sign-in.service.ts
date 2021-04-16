c6dd1b7b9e9dd2eb7990be1c68499298c4c7d028
Alex <siricuster@gmail.com>
2021-04-16 13:08:24 -0400

    auth

---
+++ src/app/modules/sign-in/sign-in.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+export interface User1 {
+  token: string;
+  refreshToken: string;
+  tokenExpiration: string;
+  id: string;
+  userName: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  status: string;
+  xp: number;
+  badgesCount: number;
+  roles: string;
+  avatarId: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class SignInService {
+  user$: BehaviorSubject<User1> = new BehaviorSubject(null as unknown as User1);
+
+  private swaggerUrl = 'https://oleksiy-tarakanov.herokuapp.com/api/authenticate';
+
+  constructor(private httpClient: HttpClient) { }
+
+  onAuthentificate(userName: string, password: string): Observable<User1> {
+    const credentials = {
+      userName,
+      password
+    };
+
+    return this.httpClient.post<User1>(`${this.swaggerUrl}`, credentials).pipe(tap(user => {
+      this.user$.next(user)
+    }));
+  }
+}
+
+
